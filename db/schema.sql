9e37f8fdd75a5c57421d8234845a1a5c55580fb8
William Dunn <pikablu112@gmail.com>
2019-11-24 14:59:59 -0800

    done

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+###SCHEMA
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burger 
+(
+    id INT auto_increment not NULL,
+    burger_name varchar(255) NOT NULL, 
+    devoured boolean NOT NULL,
+    PRIMARY KEY (id)
+);
